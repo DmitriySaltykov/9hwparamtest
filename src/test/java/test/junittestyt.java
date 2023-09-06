b12dfb96419cc2207a7a5acffbc1988cf941f0bb
DSaltykov <spitfiredia@gmail.com>
2023-09-06 09:23:14 +0300

    add change registr

--- src/test/java/test/junittestyt.java
+++ src/test/java/test/junittestyt.java
@@ -1,92 +1,88 @@
 package test;
 
-        import com.codeborne.selenide.Configuration;
-        import org.junit.jupiter.api.BeforeAll;
-        import org.junit.jupiter.api.DisplayName;
-        import org.junit.jupiter.api.Tag;
-        import org.junit.jupiter.api.Tags;
-        import org.junit.jupiter.params.ParameterizedTest;
-        import org.junit.jupiter.params.provider.Arguments;
-        import org.junit.jupiter.params.provider.CsvSource;
-        import org.junit.jupiter.params.provider.MethodSource;
-        import org.junit.jupiter.params.provider.ValueSource;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-        import java.util.List;
-        import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Stream;
 
-        import static com.codeborne.selenide.Condition.text;
-        import static com.codeborne.selenide.Selenide.$;
-        import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
-       public class junittestyt  {
+public class Junittestyt {
 
-           @BeforeAll
-           static void beforeAll() {
-             Configuration.pageLoadStrategy = "eager";
-           }
+    @BeforeAll
+    static void beforeAll() {
+        open("https://www.youtube.com/");
+        Configuration.pageLoadStrategy = "eager";
+    }
 
-        @Tags({
+    @Tags({
             @Tag("web"),
             @Tag("search")
-        })
-        @DisplayName("Проверка наличия категорий на странице youtoobe")
-        @ValueSource(strings = {"Главная" , "Shorts", "Подписки"})
+    })
+    @DisplayName("Проверка наличия категорий на странице youtoobe")
+    @ValueSource(strings = {"Главная", "Shorts", "Подписки"})
 
-        @ParameterizedTest(name = "Youtube home page should have \"{0}\" categories.")
+    @ParameterizedTest(name = "Youtube home page should have \"{0}\" categories.")
+    void ytHomePageTest(String testData) {
 
-               void ytHomePageTest (String testData) {
 
-        open("https://www.youtube.com/");
         $(".style-scope ytd-guide-renderer").shouldHave(text(testData));
 
-        }
-        @Tags({
+    }
+
+    @Tags({
             @Tag("web"),
             @Tag("search")
-        })
-        @DisplayName("Проверка наличия описания на странице канала")
+    })
+    @DisplayName("Проверка наличия описания на странице канала")
 
-        @CsvSource(value = {
-        "QA GURU, Школа инженеров по автоматизации тестирования",
-        "Academeg, Итак ....  Меня зовут Костик, и я имею свой субъективный взгляд на автомобили"
-        })
-        @ParameterizedTest(name = "Youtube chanel  \"{0}\" should have text \"{1}\" in descreption.")
-        void ytHomePageDiscreptionTest(String testData, String expectedResult) {
+    @CsvSource(value = {
+            "QA GURU, Школа инженеров по автоматизации тестирования",
+            "Academeg, Итак ....  Меня зовут Костик, и я имею свой субъективный взгляд на автомобили"
+    })
+    @ParameterizedTest(name = "Youtube chanel  \"{0}\" should have text \"{1}\" in descreption.")
+    void ytHomePageDiscreptionTest(String testData, String expectedResult) {
 
-        open("https://www.youtube.com/");
+
         $("#search-input #search").val(testData).pressEnter();
         $(".style-scope ytd-channel-renderer").click();
         $(".style-scope ytd-channel-tagline-renderer").click();
         $("#description-container").shouldHave(text(expectedResult));
 
-        }
-        @Tags({
-        @Tag("web"),
-        @Tag("search")
-        })
-        @DisplayName("Проверка наличия табов странице канала")
-        static Stream<Arguments> ytJavaProvider() {
-        return Stream.of(
-        Arguments.of("QA GURU", List.of("Главная", "Видео", "Плейлисты", "Сообщество")),
-        Arguments.of("Academeg", List.of("Главная", "Видео", "Трансляции", "Плейлисты", "Сообщество"))
-        );
-        }
-        @MethodSource("ytJavaProvider")
-        @ParameterizedTest(name = "Youtube chanel  \\\"{0}\\\" should have link \\\"{1}\\\" on Links descreption.\"")
-        void ytJavaProvider(String java, List<String> characteristic) {
+    }
 
-        open("https://www.youtube.com/");
+    @Tags({
+            @Tag("web"),
+            @Tag("search")
+    })
+    @DisplayName("Проверка наличия табов странице канала")
+    static Stream<Arguments> ytJavaProvider() {
+        return Stream.of(
+                Arguments.of("QA GURU", List.of("Главная", "Видео", "Плейлисты", "Сообщество")),
+                Arguments.of("Academeg", List.of("Главная", "Видео", "Трансляции", "Плейлисты", "Сообщество"))
+        );
+    }
+
+    @MethodSource("ytJavaProvider")
+    @ParameterizedTest(name = "Youtube chanel  \\\"{0}\\\" should have link \\\"{1}\\\" on Links descreption.\"")
+    void ytJavaProvider(String java, List<String> characteristic) {
 
 
         $("#search-input #search").val(java).pressEnter();
         $(".style-scope ytd-channel-renderer").click();
 
         $("#tabsContainer").shouldHave(text(String.join(" ", characteristic)));
-        }
-
-
-
-
-
-        }
+    }
+}
